c8f37b270914013114f34e247b8ae30ee7f582e0
Usman Afzal <usman.afzal@brainxtech.com>
2021-04-21 14:26:21 +0500

    Company api created

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Illuminate\Support\Facades\Auth;
 
 class QuestionController extends Controller
 {
@@ -20,12 +21,15 @@ class QuestionController extends Controller
     public function create()
     {
         $validatedAttributes = request()->validate([
-            'question' => 'required',
-            'description' => 'required | exists:descriptions,id',
+            'question' => 'String | required',
+            'description' => 'String |required',
             'no_thumbs_up' => 'required | integer',
             'no_thumbs_down' => 'required | integer',
         ]);
 
+        $user = auth('api')->user();
+        array_push($validatedAttributes, $user);
+        return response()->json(['attributes' => $validatedAttributes]);
         Question::create($validatedAttributes);
         return response()->json(['success' => ['Question has been created successfully'], 200], 200);
     }
